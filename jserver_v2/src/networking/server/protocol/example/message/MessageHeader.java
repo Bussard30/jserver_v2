57dba386151a9fef63432a9e96e12331965e7ef2
Jonas <j-senger@gmx.de>
2019-12-20 00:21:05 +0100

    Commit.

---
+++ jserver_v2/src/networking/server/protocol/example/message/MessageHeader.java
@@ -0,0 +1,16 @@
+package networking.server.protocol.example.message;
+
+import networking.server.protocol.message.Header;
+import networking.types.Packet;
+
+public class MessageHeader extends Header
+{
+
+	@Override
+	public String getString(Packet p)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
