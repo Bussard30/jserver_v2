b95e8144e1d18a47c95a80730b49f256a616ecb7
Bussard30 <j-senger@gmx.de>
2020-12-16 17:15:33 +0100

    Basic events.

---
+++ jserver_v2/src/de/bussard30/jserverv2/java/eventhandling/events/Event.java
@@ -0,0 +1,15 @@
+package de.bussard30.jserverv2.java.eventhandling.events;
+
+public abstract class Event {
+    protected Result r;
+
+    public abstract String getName();
+
+    public void setResult(Result r) {
+        this.r = r;
+    }
+
+    public Result getResult() {
+        return r;
+    }
+}
---
+++ jserver_v2/src/de/bussard30/jserverv2/java/eventhandling/events/MessageReceivedEvent.java
@@ -0,0 +1,9 @@
+package de.bussard30.jserverv2.java.eventhandling.events;
+
+public class MessageReceivedEvent extends Event{
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
